09d6a65de0195ff8913259bbaa01374851cb94dd
Ahhhhhhyeong <dkdud081@gmail.com>
2022-06-15 15:16:54 +0900

    Explicity Configuration(xml, Java)

---
+++ container/src/main/java/com/douzone/container/videosystem/DVDPlayer.java
@@ -0,0 +1,21 @@
+package com.douzone.container.videosystem;
+
+public class DVDPlayer {
+	
+	private DigitalVideoDisc dvd;
+	
+	public DVDPlayer() {
+	}
+	
+	public void setDvd(DigitalVideoDisc dvd) {
+		this.dvd = dvd;
+	}
+
+	public DVDPlayer(DigitalVideoDisc dvd) {
+		this.dvd = dvd;
+	}
+	
+	public String play() {
+		return dvd.play();
+	}
+}
