46a502fa4352e02de87d0b5eb1da319f48aa6fc9
Ahhhhhhyeong <dkdud081@gmail.com>
2022-06-16 11:23:38 +0900

    container #finish

--- container/src/test/java/com/douzone/container/config/videosystem/DVDPlayerXmlConfigTest.java
+++ container/src/test/java/com/douzone/container/config/videosystem/DVDPlayerXmlConfigTest.java
@@ -1,5 +1,6 @@
 package com.douzone.container.config.videosystem;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Disabled;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.douzone.container.videosystem.DVDPack;
+import com.douzone.container.videosystem.DVDPlayer;
 import com.douzone.container.videosystem.DigitalVideoDisc;
 
 @ExtendWith(SpringExtension.class)
@@ -45,10 +48,48 @@ public class DVDPlayerXmlConfigTest {
 	@Qualifier("avengersDirectorEdition")
 	private DigitalVideoDisc dvd7;
 
+	@Autowired
+	private DVDPack dvdPack;
+	
+	@Autowired
+	@Qualifier("avengersExpansionEdition")
+	private DigitalVideoDisc dvd8;
+	
+	
+	@Autowired
+	@Qualifier("avengersExpansionEdition2")
+	private DigitalVideoDisc dvd9;
+	
+	@Autowired
+	@Qualifier("avengersExpansionEdition3")
+	private DigitalVideoDisc dvd10;
+	
+	@Autowired
+	@Qualifier("dvdPlayer1")
+	private DVDPlayer dvdPlayer1;
+	
+	@Autowired
+	@Qualifier("dvdPlayer2")
+	private DVDPlayer dvdPlayer2;
+	
+	@Autowired
+	@Qualifier("dvdPlayer3")
+	private DVDPlayer dvdPlayer3;
+	
+	@Autowired
+	@Qualifier("dvdPlayer4")
+	private DVDPlayer dvdPlayer4;
+	
+	@Autowired
+	@Qualifier("dvdPlayer5")
+	private DVDPlayer dvdPlayer5;
+
+	
+	/***************************************/
 
 	@Disabled
 	@Test
-	public void testDVD1() {
+	public void testDVD() {
 		assertNotNull(dvd1);
 	}
 
@@ -82,4 +123,32 @@ public class DVDPlayerXmlConfigTest {
 		assertNotNull(dvd7);
 	}
 
+	@Test
+	public void testDVDPack1() {
+		assertNotNull(dvdPack);
+	}
+	
+	@Test
+	public void testDVD8() {
+		assertNotNull(dvd8);
+	}
+
+	@Test
+	public void testDVD9() {
+		assertNotNull(dvd9);
+	}
+	
+	@Test
+	public void testDVD10() {
+		assertNotNull(dvd10);
+	}
+
+	@Test
+	public void testDVDPlayer() {
+		assertEquals("Playing Movie MARVEL's Iron Man", dvdPlayer1);
+		assertEquals("Playing Movie MARVEL's Iron Man", dvdPlayer2);
+		assertEquals("Playing Movie MARVEL's Iron Man", dvdPlayer3);
+		assertEquals("Playing Movie MARVEL's Iron Man", dvdPlayer4);
+		assertEquals("Playing Movie MARVEL's Iron Man", dvdPlayer5);
+	}
 } 
